e85e9be4f3c7d3473e1f1b25887c4c72969e1ee2
Basty0 <126388152+Basty0@users.noreply.github.com>
2024-09-04 13:15:02 +0300

    ilfaut voir les modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,15 @@ export default function Home() {
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
         <div className="bg-white p-5 rounded-full ">
-          <h1 className="text-6xl text-zinc-900 font-bold">Lycée Augustin</h1>
+          <h1 className="text-6xl text-zinc-900 font-bold">
+            Lycée privée Augustin
+          </h1>
+          <div className="w-full aspect-square">
+            <p>
+              Lycée privée Augustin est une lycée de tout le monde.
+              <br />
+            </p>
+          </div>
         </div>
       </main>
     </div>
